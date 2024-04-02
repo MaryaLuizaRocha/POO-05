dc8cbf00254a7d7a04ccec741ef788ad661bb0f3
Marya Luiza Rocha <84876774+MaryaLuizaRocha@users.noreply.github.com>
2024-04-01 23:14:36 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,21 @@
+public class Carro extends Automovel {
+    private int numPortas;
+
+    public Carro(String marca, String modelo, int ano, int numPortas) {
+        super(marca, modelo, ano);
+        this.numPortas = numPortas;
+    }
+
+    public int getNumPortas() {
+        return numPortas;
+    }
+
+    public void setNumPortas(int numPortas) {
+        this.numPortas = numPortas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Número de portas: " + numPortas;
+    }
+}
---
+++ TesteVeiculo.java
@@ -0,0 +1,10 @@
+public class TesteVeiculo {
+
+    public static void main(String[] args) {
+        Carro carroA = new Carro("FIAT", "PULSE", 2024, 4);
+        Moto motoB = new Moto("Yamaha", "CBR500R", 2023, false);
+
+        System.out.println(carroA);
+        System.out.println(motoB);
+    }
+}
